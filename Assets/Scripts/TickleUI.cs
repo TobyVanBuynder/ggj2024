7c8becd55c3cebdbd106f74aeee7af566800e2b9
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-01-27 16:33:21 +0100

    TickleUI initial setup

--- Assets/Scripts/TickleUI.cs
+++ Assets/Scripts/TickleUI.cs
@@ -11,25 +11,44 @@ public class TickleUI : MonoBehaviour
         A,
         B,
         X,
-        Y
+        Y,
+        MAX
     }
     private Tickle _currentTickle;
     private List<TickleButton> _sequence;
+    private float _timeToComplete;
+    private const float _baseTimeToComplete = 10.0f;
+    [SerializeField] private int _numButtonsModifier = 6;
 
     public bool IsOpen{get; private set;}
 
     void Awake()
     {
+        _sequence = new List<TickleButton>();
         IsOpen = false;
     }
 
-    public void Open(int difficulty)
+    public void Open(Tickle tickle)
     {
+        _currentTickle = tickle;
+        SetupSequence(_currentTickle.Difficulty);
         IsOpen = true;
     }
 
     public void Close()
     {
         IsOpen = false;
+    }
+
+    private void SetupSequence(int difficulty)
+    {
+        _sequence.Clear();
+
+        int sequenceSize = difficulty * _numButtonsModifier;
+        _sequence.Capacity = sequenceSize;
+        for (int b = 0; b < sequenceSize; b++)
+        {
+            _sequence.Add((TickleButton)Random.Range(0, (int)TickleButton.MAX));
+        }
     }
 }
