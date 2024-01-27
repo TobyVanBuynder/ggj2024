718cd234929448c59760a0b426b67d2732e961b3
Ciro Continisio <c.continisio@gmail.com>
2024-01-27 14:42:20 +0100

    Test collisions

---
+++ Assets/Scripts/Dragon.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+    private int mood = 0; // 0 = mega-grumpy, more = happier
+}
